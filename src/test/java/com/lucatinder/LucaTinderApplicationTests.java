187f5b3932d68a2e028bec8a32c6f50df8797764
biorubenfs <65553496+biorubenfs@users.noreply.github.com>
2020-06-03 09:19:41 +0200

    Add files via upload
    Se sube la carpeta src y el pom.xml del proyecto Spring creado en Eclipse. #Ruben#

---
+++ src/test/java/com/lucatinder/LucaTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
