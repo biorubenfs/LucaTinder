bf291b7b555bf493bd8d739e86c45feb20e98df9
biorubenfs <biorubenfs@hotmail.com>
2020-06-08 09:22:37 +0200

    Metodo REST de alta
    Metodo REST de alta

--- src/main/java/com/lucatinder/control/ControlRest.java
+++ src/main/java/com/lucatinder/control/ControlRest.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +15,18 @@ import com.lucatinder.servicios.PerfilServicios;
 @RestController
 @RequestMapping("/rperfil")
 public class ControlRest {
-	
+
 	@Autowired
 	private PerfilServicios perfilServicios;
-	
+
 	@GetMapping("/listar")
 	public Collection<Perfil> listarPerfiles() {
 		return perfilServicios.listarPerfil();
+	}
+
+	@PostMapping("/alta")
+	public Perfil agregarPerfil(@RequestBody Perfil perfil) {
+		perfilServicios.agregarPerfil(perfil);
+		return perfil;
 	}
 }
