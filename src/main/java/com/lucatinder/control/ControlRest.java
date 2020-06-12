017ce1c069c5b5b6c8201a4c45336d535a9bdfba
alejandrojr98 <alejandrojr2011@gmail.com>
2020-06-12 12:16:43 +0200

    Metodo rest
    metodo

--- src/main/java/com/lucatinder/control/ControlRest.java
+++ src/main/java/com/lucatinder/control/ControlRest.java
@@ -6,11 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucatinder.modelo.Contacto;
 import com.lucatinder.modelo.Perfil;
 import com.lucatinder.servicios.PerfilServicios;
 
@@ -35,5 +37,12 @@ public class ControlRest {
 		logger.info(">>>>>>>> agregado perfil REST  ");
 		perfilServicios.agregarPerfil(perfil);
 		return perfil;
+	}
+	
+	@GetMapping("/listarContactos/{idContacto}")
+	public Collection<Contacto> listarContactos(@PathVariable("idContacto") int id_perfil){
+		logger.info(">>>>>>>> listado contacto REST /");
+		System.out.println(id_perfil);
+		return perfilServicios.listarContactos(id_perfil);
 	}
 }
